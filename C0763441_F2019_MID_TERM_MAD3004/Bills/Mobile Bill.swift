62ae419e28d92647b571a9e8a26438af58dc0589
Ajeet Singh <ajeets110@gmail.com>
2019-10-19 18:45:53 -0400

    Declared variables of mobile class

---
+++ C0763441_F2019_MID_TERM_MAD3004/Bills/Mobile Bill.swift
@@ -0,0 +1,21 @@
+//
+//  Mobile Bill.swift
+//  C0763441_F2019_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-10-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class MobileBill{
+    
+    //  Variable declaration
+    var manufacturerName : String
+    var planName : String
+    var phoneNumber : Int
+    var minutesUsed : Int
+    var internetUsed : Float
+    var billAmount : Float
+    
+    
+}
